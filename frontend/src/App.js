54f95ff9bd3b1b993e42c38976003f99d64ddaac
bbungalan@gmail.com <bbungalan@gmail.com>
2023-06-15 19:23:18 +0800

    add vercel.json file for vercel deployment requirement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
-import "./Styles/App.css";
-
 import RegistrationForm from "./Components/signup";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>My App</h1>
-        <RegistrationForm />
-      </div>
+    <div>
+      <RegistrationForm />
     </div>
   );
 }
