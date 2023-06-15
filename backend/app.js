0eebea634624124a2c1604df08db990f55869497
bbungalan@gmail.com <bbungalan@gmail.com>
2023-06-15 16:18:00 +0800

    initialize project

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3003; // Specify the port you want to use
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./Styles/App.css";
+
+import RegistrationForm from "./Components/signup";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>My App</h1>
+        <RegistrationForm />
+      </div>
+    </div>
+  );
+}
+
+export default App;
