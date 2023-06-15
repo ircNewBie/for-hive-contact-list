f21b85eb0745483013fa3b7aade488be5b2dbd1c
bbungalan@gmail.com <bbungalan@gmail.com>
2023-06-15 20:47:46 +0800

    revise api enry file

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3003; // Specify the port you want to use
+const port = 5000; // Specify the port you want to use
 
 // Middleware
 app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("ContactList App v0.1");
 });
 
 // Start the server
