cf2cb57f4abf399e07d8774cfdaade82aa2a241a
bbungalan@gmail.com <bbungalan@gmail.com>
2023-06-19 20:22:02 +0800

    created seeder

--- backend/src/db/seeder.js
+++ backend/src/db/seeder.js
@@ -1,1 +1,35 @@
-// @TODO
+require("dotenv").config();
+
+const userSeederRun = require("./seeders/user.seeder");
+
+const mongoose = require("mongoose");
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_DBASE = process.env.MONGODB_DB;
+
+const MONGO_CONN_STRING =
+  process.env.MONGODB_URI_STAGING || MONGODB_URI + MONGODB_DBASE;
+
+const runSeeders = async () => {
+  try {
+    await mongoose.connect(MONGO_CONN_STRING, {
+      useNewUrlParser: true,
+    });
+
+    await userSeederRun();
+    // await regionSeederRun();
+    // await provinceSeederRun();
+    // await cityMunicipalitySeederRun();
+    // await cityMunicipalityBarangaysSeederRun();
+    // await purokSeedRun();
+
+    console.log("All seeders finished successfully.");
+    mongoose.disconnect();
+  } catch (error) {
+    console.error("Error running seeders:", error);
+    mongoose.disconnect();
+    process.exit(1);
+  }
+};
+
+runSeeders();
