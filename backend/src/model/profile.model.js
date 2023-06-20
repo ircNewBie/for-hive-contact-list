f614f6aaaa26737a11032b6bd9d0c1ec8a5766f8
bbungalan@gmail.com <bbungalan@gmail.com>
2023-06-20 11:50:39 +0800

    create profile model

---
+++ backend/src/model/profile.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  aboutBio: { type: String },
+  skills: [{ type: String }],
+  hobbies: [{ type: String }],
+  profession: { type: String },
+  profilePhoto: {
+    type: String,
+    default: "https://publicdomainvectors.org/photos/generic-avatar.png",
+  },
+  socialMediaLinks: [{ type: Object }],
+});
+
+const Profile = mongoose.model("Profile", profileSchema);
+
+module.exports = Profile;
